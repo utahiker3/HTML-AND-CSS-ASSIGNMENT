0532d64e72852a1e1e9ce2a9a611fd97a1b51f34
utahiker3 <brycespalding406@gmail.com>
2024-04-23 17:26:38 -0600

    added basic HTML file

---
+++ Basic_Javascript_1.js
@@ -0,0 +1,16 @@
+<DOCTYPE javascript>
+var Dog = (
+	firstname: "Carl"
+	lastname: "Spalding"
+	Color: "mulitple"
+	Age: "almost 2"
+)
+
+function name(peepee_poopoo)
+grade average: function(avg) {
+function avg;
+	return 1+2
+}
+var sum = add(5, 7);
+};
+
